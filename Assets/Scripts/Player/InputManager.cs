7f4f22dfed0658cafa9f8f8b65306d50df88b6f2
Melon332 <thederpking@outlook.com>
2021-03-11 10:48:39 +0100

    Fixed the player movement and added some colors to the scene

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public event EventHandler<(float, float)> movement;
+
+    float vertical, horizontal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        InputMovement();
+    }
+
+    public void InputMovement()
+    {
+        if (vertical != 0 || horizontal != 0)
+        {
+            movement?.Invoke(this, (vertical, horizontal));
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    CharacterController controller;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    [SerializeField] float speed;
+
+    float gravity = 5;
+
+    InputManager inputs;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+        Debug.Log(controller.isGrounded);
+
+        speed = 15;
+
+        inputs = GetComponent<InputManager>();
+        inputs.movement += Inputs_movement;
+    }
+    private void Update()
+    {
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Inputs_movement(object sender, (float, float) e)
+    {
+        Move(e.Item1, e.Item2);
+        Debug.Log("I am being called!");
+    }
+
+    public void Move(float vertical, float horizontal)
+    {
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(horizontal, 0, vertical);
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            Debug.Log(moveDirection);
+        }
+    }
+}
