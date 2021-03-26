316dfe2aa8a45105b06ad47815f361a37a7621c3
Melon332 <thederpking@outlook.com>
2021-03-26 09:25:06 +0100

    Added a FPS counter and removed a minimaptest shader

---
+++ Assets/Scripts/FPSLocker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSLocker : MonoBehaviour
+{
+    [SerializeField] Text fpsText;
+
+    int avgFrameRate;
+
+    public int targetFrameRate = 144;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+    public void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        fpsText.text = avgFrameRate.ToString() + " FPS";
+    }
+}
